39a63d76c5a62e47249a5e6b1184c418c3aed2db
Ziv Cohen <zivcon1@gmail.com>
2026-04-16 15:13:02 -0400

    Complete ast and cfg

--- modules/module2-ast/exercises/ast-transformations/starter/transformations.ml
+++ modules/module2-ast/exercises/ast-transformations/starter/transformations.ml
@@ -28,8 +28,42 @@ open Shared_ast.Ast_types
      BinOp(Add, Var "x", IntLit 1)            --> BinOp(Add, Var "x", IntLit 1)
 *)
 let constant_fold (_expr : expr) : expr =
-  (* TODO *)
-  failwith "TODO"
+  let rec fold_expr (expr : expr) : expr =
+    match expr with
+    | IntLit _ | BoolLit _ | Var _ -> expr
+    | Call (fname, args) -> Call (fname, List.map fold_expr args)
+    | UnaryOp (Neg, e) ->
+      begin match fold_expr e with
+      | IntLit n -> IntLit (-n)
+      | e' -> UnaryOp (Neg, e')
+      end
+    | UnaryOp (Not, e) ->
+      begin match fold_expr e with
+      | BoolLit b -> BoolLit (not b)
+      | e' -> UnaryOp (Not, e')
+      end
+    | BinOp (op, l, r) ->
+      let l' = fold_expr l in
+      let r' = fold_expr r in
+      begin match op, l', r' with
+      | Add, IntLit a, IntLit b -> IntLit (a + b)
+      | Sub, IntLit a, IntLit b -> IntLit (a - b)
+      | Mul, IntLit a, IntLit b -> IntLit (a * b)
+      | Div, IntLit a, IntLit b when b <> 0 -> IntLit (a / b)
+      | Eq,  IntLit a, IntLit b -> BoolLit (a = b)
+      | Neq, IntLit a, IntLit b -> BoolLit (a <> b)
+      | Lt,  IntLit a, IntLit b -> BoolLit (a < b)
+      | Gt,  IntLit a, IntLit b -> BoolLit (a > b)
+      | Le,  IntLit a, IntLit b -> BoolLit (a <= b)
+      | Ge,  IntLit a, IntLit b -> BoolLit (a >= b)
+      | Eq,  BoolLit a, BoolLit b -> BoolLit (a = b)
+      | Neq, BoolLit a, BoolLit b -> BoolLit (a <> b)
+      | And, BoolLit a, BoolLit b -> BoolLit (a && b)
+      | Or,  BoolLit a, BoolLit b -> BoolLit (a || b)
+      | _ -> BinOp (op, l', r')
+      end
+  in
+  fold_expr _expr
 
 (* --------------------------------------------------------------------------
    2. Variable renaming
@@ -50,8 +84,29 @@ let constant_fold (_expr : expr) : expr =
 *)
 let rename_variable (_old_name : string) (_new_name : string)
     (_stmts : stmt list) : stmt list =
-  (* TODO *)
-  failwith "TODO"
+  let rename_name name = if String.equal name _old_name then _new_name else name in
+  let rec rename_expr (expr : expr) : expr =
+    match expr with
+    | IntLit _ | BoolLit _ -> expr
+    | Var name -> Var (rename_name name)
+    | UnaryOp (op, e) -> UnaryOp (op, rename_expr e)
+    | BinOp (op, l, r) -> BinOp (op, rename_expr l, rename_expr r)
+    | Call (fname, args) -> Call (fname, List.map rename_expr args)
+  in
+  let rec rename_stmt (stmt : stmt) : stmt =
+    match stmt with
+    | Assign (name, e) -> Assign (rename_name name, rename_expr e)
+    | If (cond, then_branch, else_branch) ->
+      If (cond |> rename_expr,
+          then_branch |> List.map rename_stmt,
+          else_branch |> List.map rename_stmt)
+    | While (cond, body) -> While (rename_expr cond, List.map rename_stmt body)
+    | Return None -> Return None
+    | Return (Some e) -> Return (Some (rename_expr e))
+    | Print exprs -> Print (List.map rename_expr exprs)
+    | Block stmts -> Block (List.map rename_stmt stmts)
+  in
+  List.map rename_stmt _stmts
 
 (* --------------------------------------------------------------------------
    3. Dead-code elimination
@@ -75,5 +130,27 @@ let rename_variable (_old_name : string) (_new_name : string)
      [Return (Some (IntLit 42))]
 *)
 let eliminate_dead_code (_stmts : stmt list) : stmt list =
-  (* TODO *)
-  failwith "TODO"
+  let rec dce_list (stmts : stmt list) : stmt list =
+    match stmts with
+    | [] -> []
+    | stmt :: rest ->
+      let stmt' = dce_stmt stmt in
+      begin match stmt' with
+      | Return _ -> [stmt']
+      | _ -> stmt' :: dce_list rest
+      end
+  and dce_stmt (stmt : stmt) : stmt =
+    match stmt with
+    | Assign _ -> stmt
+    | Print _ -> stmt
+    | Return _ -> stmt
+    | While (cond, body) -> While (cond, dce_list body)
+    | Block stmts -> Block (dce_list stmts)
+    | If (BoolLit true, then_branch, _else_branch) ->
+      Block (dce_list then_branch)
+    | If (BoolLit false, _then_branch, else_branch) ->
+      Block (dce_list else_branch)
+    | If (cond, then_branch, else_branch) ->
+      If (cond, dce_list then_branch, dce_list else_branch)
+  in
+  dce_list _stmts
--- modules/module3-static-analysis/exercises/cfg-construction/starter/cfg.ml
+++ modules/module3-static-analysis/exercises/cfg-construction/starter/cfg.ml
@@ -33,22 +33,35 @@ let add_edge (cfg : cfg) (src : string) (dst : string) : cfg =
      3. All other blocks remain unchanged
      Hint: Look up both blocks in cfg.blocks using StringMap.find,
      create updated copies, and build a new blocks map with StringMap.add. *)
-  ignore (cfg, src, dst);
-  failwith "TODO: add_edge"
+  let src_block = StringMap.find src cfg.blocks in
+  let dst_block = StringMap.find dst cfg.blocks in
+  if String.equal src dst then
+    let updated_block = {
+      src_block with
+      succs = src_block.succs @ [dst];
+      preds = src_block.preds @ [src];
+    }
+    in
+    { cfg with blocks = StringMap.add src updated_block cfg.blocks }
+  else
+    let updated_src = { src_block with succs = src_block.succs @ [dst] } in
+    let updated_dst = { dst_block with preds = dst_block.preds @ [src] } in
+    let blocks = cfg.blocks |> StringMap.add src updated_src |> StringMap.add dst updated_dst in
+    { cfg with blocks }
 
 let predecessors (cfg : cfg) (label : string) : string list =
   (* TODO: Look up the block with the given label in cfg.blocks
      and return its preds list.
      Hint: Use StringMap.find. *)
-  ignore (cfg, label);
-  failwith "TODO: predecessors"
+  let block = StringMap.find label cfg.blocks in
+  block.preds
 
 let successors (cfg : cfg) (label : string) : string list =
   (* TODO: Look up the block with the given label in cfg.blocks
      and return its succs list.
      Hint: Use StringMap.find. *)
-  ignore (cfg, label);
-  failwith "TODO: successors"
+  let block = StringMap.find label cfg.blocks in
+  block.succs
 
 let to_string (cfg : cfg) : string =
   (* TODO: Build a human-readable string representation of the CFG.
@@ -61,5 +74,17 @@ let to_string (cfg : cfg) : string =
 
      Hint: Use StringMap.fold to iterate over cfg.blocks.
      Use String.concat to join lists of labels. *)
-  ignore cfg;
-  failwith "TODO: to_string"
+  let pp_labels labels = String.concat ", " labels in
+  let pp_block _label block acc =
+    let block_str =
+      Printf.sprintf "Block: %s (%d stmts)\n  succs: [%s]\n  preds: [%s]"
+        block.label
+        (List.length block.stmts)
+        (pp_labels block.succs)
+        (pp_labels block.preds)
+    in
+    block_str :: acc
+  in
+  StringMap.fold pp_block cfg.blocks []
+  |> List.rev
+  |> String.concat "\n\n"
